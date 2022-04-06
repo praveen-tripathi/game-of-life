3ca707445fecbf8c904abb468042bd1840606d26
Praveen Tripathi <pktripathi289@gmail.com>
2022-04-06 11:10:32 +0530

    Game simulation with start and clear control added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useState, useCallback, useRef } from "react";
+import useInterval from "./useInterval";
 
-function App() {
+const numRows = 50;
+const numCols = 50;
+
+const positions = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+const generateEmptyGrid = (): number[][] => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+  return rows;
+};
+
+const randomTiles = (): number[][] => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0)));
+  }
+  return rows;
+};
+
+const App: FC = () => {
+  const [grid, setGrid] = useState(() => {
+    return randomTiles();
+  });
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback((grid) => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    let gridCopy = JSON.parse(JSON.stringify(grid));
+    for (let i = 0; i < numRows; i++) {
+      for (let j = 0; j < numCols; j++) {
+        let neighbors = 0;
+
+        positions.forEach(([x, y]) => {
+          const newI = i + x;
+          const newJ = j + y;
+
+          if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+            neighbors += grid[newI][newJ];
+          }
+        });
+
+        if (neighbors < 2 || neighbors > 3) {
+          gridCopy[i][j] = 0;
+        } else if (grid[i][j] === 0 && neighbors === 3) {
+          gridCopy[i][j] = 1;
+        }
+      }
+    }
+
+    setGrid(gridCopy);
+  }, []);
+
+  useInterval(() => {
+    runSimulation(grid);
+  }, 150);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 10px)`,
+          width: "fit-content",
+          margin: "0 auto",
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, j) => (
+            <div
+              style={{
+                width: 10,
+                height: 10,
+                backgroundColor: grid[i][j] ? "#F68E5F" : undefined,
+                border: "1px solid #595959",
+              }}
+              key={`${i}-${j}`}
+              onClick={() => {
+                let newGrid = JSON.parse(JSON.stringify(grid));
+                newGrid[i][j] = grid[i][j] ? 0 : 1;
+                setGrid(newGrid);
+              }}
+            />
+          ))
+        )}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          padding: 10,
+        }}
+      >
+        <button
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {running ? "Stop" : "Start"}
+        </button>
+        <button
+          onClick={() => {
+            const gridG = generateEmptyGrid();
+            setGrid(gridG);
+          }}
+        >
+          Clear board
+        </button>
+        <button
+          onClick={() => {
+            setGrid(randomTiles());
+          }}
+        >
+          Random
+        </button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
